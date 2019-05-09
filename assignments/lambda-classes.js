47b90bbca77b25e9898382abd2ffdf218463b0f9
fromthehart <fromthehart@pm.me>
2019-05-09 14:57:26 -0400

    lambda-classes.js: class Student complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -14,8 +14,8 @@ class Person {
   }
 }
 
-const michaelhart = new Person({name: 'Michael Hart', age: 43, location: 'MD'});
-michaelhart.speak();
+const michaelhartPerson = new Person({name: 'Michael Hart', age: 43, location: 'MD'});
+michaelhartPerson.speak();
 
 class Instructor extends Person {
   constructor({name, age, location, specialty, favLanguage, catchPhrase}) {
@@ -37,4 +37,40 @@ class Instructor extends Person {
 const danlevy = new Instructor({name: 'Dan Levy', age: 'unknown', location: 'Lambda', specialty: 'CS', favLanguage: 'Javascript', catchPhrase: "You'll get there"});
 danlevy.speak();
 danlevy.demo('Closures');
-danlevy.grade(michaelhart, 'Prototypes');
+
+class Student extends Person {
+  constructor({name, age, location, previousBackground, className, favSubjects}) {
+    super({name, age, location});
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubjects = favSubjects;
+  }
+
+  listsSubjects() {
+    console.log('Favorite subjects:');
+    this.favSubjects.forEach(subject => console.log(subject));
+  }
+
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`);
+  }
+
+  sprintChallenge(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
+  }
+}
+
+const michaelhartStudent = new Student({
+  name: 'Michael Hart',
+  age: 43,
+  location: 'MD',
+  previousBackground: 'Caretaking',
+  className: 'WEB20',
+  favSubjects: ['Python', 'Javascript', 'Lunch']
+});
+
+michaelhartStudent.speak();
+michaelhartStudent.listsSubjects();
+michaelhartStudent.PRAssignment('Python');
+michaelhartStudent.sprintChallenge('Javascript');
+danlevy.grade(michaelhartStudent, 'Prototypes');
